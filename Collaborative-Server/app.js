7f907445d96492c20812ab6758e383e93fe42145
ShreyaKeni <65093019+ShreyaKeni@users.noreply.github.com>
2020-05-13 14:13:45 -0400

    Update app.js

--- Collaborative-Server/app.js
+++ Collaborative-Server/app.js
@@ -7,53 +7,28 @@ var urlParser = require('url');
 
 var app = express();
 
-/**
- * middle-wares
- * bodyParser.json(): parses request-body with header field "Content-type" set to "application/json" to Javascript-Object
-**/
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-/**
- * Path of directory where user-projects are saved
- **/
+
 var dirName = __dirname + '\\UserProjects';
 
-/**
- * Handlers for Requests
- *
- * @requires auth: handles authentication requests
- * @requires projectManager: handles directory and file management requests
- * @requires share: handles project sharing and viewing requests
- * @requires notification: handles view and clear notification request
- * @requires docHandler: handles document-editing request namely REGISTER, UN-REGISTER, GET and PUSH
- *
-**/
+
 var auth = require('./auth');
 var projectManager = require('./projectManager');
 var notification = require('./notification');
 var share = require('./shareProject');
 var docHandler = require('./session_manager');
 
-/**
- * Session Manager to handle Document-Editing requests
-**/
+
 var sessionManager = new docHandler.SessionManager();
 
-/**
- * Establish database-connection
-**/
+
 mongoose.connect('mongodb://localhost:27017');
 var conn = mongoose.connection;
-/**
- * Routes requests to appropriate handlers
- *
- **/
 
-/**
- * Document-Editing Request-Handler
- **/
 app.use("/register", function(req, res) {
     var parsedQuery = extractQuery(req.url);
 
@@ -77,9 +52,7 @@ app.use("/push_operation", function(req, res) {
     sessionManager.handlePush(req, res, parsedQuery.userId, parsedQuery.docId);
 })
 
-/**
- * Project/Doc Creation and Management Request-Handler
- **/
+
 app.use("/view", function(req, res) {
     var parsedQuery = extractQuery(req.url);
     projectManager.view(req, res, parsedQuery.path);
@@ -95,9 +68,7 @@ app.use("/add_node", function(req, res) {
     projectManager.addNode(req, res, parsedQuery.path);
 })
 
-/**
- * Authentication Request-Handler
- **/
+
 app.use("/login", function(req, res) {
     var parsedQuery = extractQuery(req.url);
     auth.logIn(req, res);
@@ -113,9 +84,7 @@ app.use("/get_users", function(req, res) {
     auth.getUsers(req, res);
 })
 
-/**
- * Share Project Request-Handler
- **/
+
 app.use("/share", function(req, res) {
     var parsedQuery = extractQuery(req.url);
     console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>req, res", req, res);
@@ -128,9 +97,7 @@ app.use("/get_shared_projects", function(req, res) {
     share.getSharedProjects(req, res, parsedQuery.userId);
 })
 
-/**
- * Notification Request-Handler
- **/
+
 app.use("/get_notifications", function(req, res) {
     var parsedQuery = extractQuery(req.url);
     notification.getNotifications(req, res, parsedQuery.userId);
@@ -141,14 +108,7 @@ app.use("/clear_notifications", function(req, res) {
     notification.clearAll(req, res, parsedQuery.userId);
 })
 
-/**
- * Utility Functions
- **/
 
-/**
- * @return {Object} Parsed Query
- * @param url {String} url of received request
- */
 function extractQuery(url) {
     var parsedURL = urlParser.parse(url);
     var query = parsedURL.query;
