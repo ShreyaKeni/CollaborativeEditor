1909baa4f60b33e36addc582d2de303196d5d886
ShreyaKeni <65093019+ShreyaKeni@users.noreply.github.com>
2020-05-13 13:43:58 -0400

    Update utility.js

--- Collaborative-Server/utility.js
+++ Collaborative-Server/utility.js
@@ -1,13 +1,10 @@
 /**
  * Utility Functions
- *
  */
  
 module.exports = {
 	/**
 	 * Perform logging, can be redirected if needed. 
-	 * @param title {String} Title of logged message.
-	 * @param desc {String} Description of logged message.
 	 */
 	DEBUG: true,
 
@@ -17,11 +14,7 @@ module.exports = {
 		}
 	},
 
-	/**
-	* fix 'End of Line' issues. 
-	* @param content {String} content whose EOL need to be fixed.
-	* @return {String} content with '\r\n' replaced by '\n'.
-	*/
+	
 	fixEOL: function (content) {
 		var newContent = '';
 		//replace method isn't working so doing this manually
